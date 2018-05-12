ac33464a47a32759012bcbde8f4cb8c503e947e1
HuangWay <wong-way@163.com>
2018-05-12 13:17:13 +0800

    动态规划

--- src/solution/Solution646.java
+++ src/solution/Solution646.java
@@ -29,4 +29,16 @@ public class Solution646 {
         return max;
 
     }
+    public int findLongestChain1(int[][] pairs) {
+        if (pairs == null || pairs.length == 0) return 0;
+        Arrays.sort(pairs, (a, b) -> (a[0] - b[0]));
+        int[] dp = new int[pairs.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < pairs.length; i++) {
+            for (int j = i+1 ;j < pairs.length; j++) {
+                dp[j] = Math.max(dp[j], pairs[j][0] > pairs[i][1]? dp[i] + 1 : dp[i]);
+            }
+        }
+        return dp[pairs.length - 1];
+    }
 }
