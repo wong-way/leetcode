ddd694288657f63b8287ecabbf08b9e26f40adb2
HuangWay <wong-way@163.com>
2018-05-22 17:37:14 +0800

    双指针

--- src/solution/Solution16.java
+++ src/solution/Solution16.java
@@ -1,26 +1,47 @@
 package solution;
 
+import java.util.Arrays;
+
 /**
  * Created by DELL on 2018-05-03.
  */
 public class Solution16 {
     public int threeSumClosest(int[] nums, int target) {
-        int minLen = Integer.MAX_VALUE;
-        int closest=-1;
-        for (int i = 1; i < nums.length-1; i++) {
-            int sum = nums[i - 1] + nums[i] + nums[i + 1];
-            int len = Math.abs(sum-target);
-            if(minLen>len) {
-                minLen=len;
-                closest =sum;
+        Arrays.sort(nums);
+        int result = Integer.MAX_VALUE+target;
+        for (int i = 1; i < nums.length; i++) {
+            int temp = getSum(nums, i, target);
+            if (Math.abs(temp - target) < Math.abs(result - target)) {
+                result = temp;
             }
+
         }
-        return closest;
+        return result;
+
+    }
+
+    public int getSum(int[] nums, int index, int target) {
+        int i = index-1;
+        int j = index+1;
+        int sum = Integer.MAX_VALUE+target;
+        while (i >= 0 && j < nums.length) {
+            int temp = nums[i] + nums[index] + nums[j];
+            System.out.println(index + " " + i + " " + j + " " + temp);
+            if (Math.abs(temp - target) < Math.abs(sum - target)) {
+                sum = temp;
+            }
+            if (temp > target) {
+                i--;
+            } else if (temp<target){
+                j++;
+            }else return target;
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 1, -1, -1,3};
+        int[] arr = {1,1,-1,-1,3};
         Solution16 solution = new Solution16();
-        solution.threeSumClosest(arr, 3);
+        System.out.println(solution.threeSumClosest(arr, -1));
     }
 }
---
+++ src/solution/Solution357.java
@@ -0,0 +1,20 @@
+package solution;
+
+/**
+ * Created by DELL on 2018-05-22.
+ */
+public class Solution357 {
+    public int countNumbersWithUniqueDigits(int n) {
+        int len = (int) Math.pow(10, 2);
+        boolean[] dp = new boolean[len];
+        for (int i = 0; i < len; i++) {
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
