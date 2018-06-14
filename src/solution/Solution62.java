f6ed748e3973c9338c1a3939d0ca9f6cdd82d8d1
HuangWay <wong-way@163.com>
2018-06-14 11:40:06 +0800

    dynamic programming

---
+++ src/solution/Solution62.java
@@ -0,0 +1,29 @@
+package solution;
+
+public class Solution62 {
+    public int uniquePaths(int m, int n) {
+        int len = m*n;
+        int [] dp = new int[len];
+        dp[0]=1;
+        int  i= 1;
+        while(i<len){
+            int row = i/m;
+            int col = i%m;
+            if(row == 0){
+                dp[i] = dp[col-1];
+            }else if(col ==0){
+                dp[i] = dp[(row-1)*m];
+            }else{
+                dp[i] = dp[i-1]+dp[(row-1)*m+col];
+            }
+
+            i++;
+        }
+        return dp[len-1];
+    }
+
+    public static void main(String[] args) {
+        Solution62 solution = new Solution62();
+        solution.uniquePaths(3, 7);
+    }
+}
