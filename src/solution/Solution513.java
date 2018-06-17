a12e9cc5e21ccf001943303c74b9446c12235e40
HuangWay <wong-way@163.com>
2018-06-17 15:18:16 +0800

    binary tree

---
+++ src/solution/Solution513.java
@@ -0,0 +1,43 @@
+package solution;
+
+import util.TreeNode;
+
+public class Solution513 {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if (left > right) {
+            return findBottomLeftValue(root.left);
+        } else return findBottomLeftValue(root.right);
+
+
+    }
+
+    private int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        else return 1 + Math.max(getDepth(root.left), getDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node3.left = node5;
+        node3.right = node6;
+        node5.left = node7;
+        Solution513 solution = new Solution513();
+        int result = solution.findBottomLeftValue(node1);
+
+        System.out.println("result = " + result);
+    }
+}
