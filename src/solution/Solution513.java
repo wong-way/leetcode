03000e584528c17cf0075c4e0cf403f6305e1033
HuangWay <wong-way@163.com>
2018-06-17 17:17:39 +0800

    binary tree

--- src/solution/Solution513.java
+++ src/solution/Solution513.java
@@ -3,6 +3,7 @@ package solution;
 import util.TreeNode;
 
 public class Solution513 {
+    int depth=5;
     public int findBottomLeftValue(TreeNode root) {
         if (root.left == null && root.right == null) {
             return root.val;
@@ -20,6 +21,18 @@ public class Solution513 {
         if (root == null) return 0;
         else return 1 + Math.max(getDepth(root.left), getDepth(root.right));
     }
+    int ans=0, h=0;
+    public int findBottomLeftValue1(TreeNode root) {
+        findBottomLeftValue(root, 1);
+        return ans;
+    }
+    public void findBottomLeftValue(TreeNode root, int depth) {
+
+        if (h<depth) {ans=root.val;h=depth;}
+        if (root.left!=null) findBottomLeftValue(root.left, depth+1);
+        if (root.right!=null) findBottomLeftValue(root.right, depth+1);
+
+    }
 
     public static void main(String[] args) {
         TreeNode node1 = new TreeNode(1);
@@ -34,9 +47,9 @@ public class Solution513 {
         node2.left = node4;
         node3.left = node5;
         node3.right = node6;
-        node5.left = node7;
+//        node5.left = node7;
         Solution513 solution = new Solution513();
-        int result = solution.findBottomLeftValue(node1);
+        int result = solution.findBottomLeftValue1(node1);
 
         System.out.println("result = " + result);
     }
